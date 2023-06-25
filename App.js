629c8edb1c4b1d8408869e796c5f21890f7fb111
Nilesh Kumar Choudhary <104972509+nileshchy@users.noreply.github.com>
2023-06-25 11:29:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Importing the CSS stylesheet
+import './styles.scss'; // Importing the SCSS stylesheet
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">This is a React app with SCSS styling!</h1>
+      <p className="description">The text in this paragraph is styled using SCSS.</p>
+    </div>
+  );
+}
+
+export default App;
